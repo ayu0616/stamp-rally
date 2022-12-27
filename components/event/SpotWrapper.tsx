2df32f3649c07d41ee7a516c1ef01a7d8cef670c
ayu0616 <ayumuogawa0616@gmail.com>
2022-12-27 23:53:20 +0900

    formatted

--- components/event/SpotWrapper.tsx
+++ components/event/SpotWrapper.tsx
@@ -1,9 +1,14 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type Props = {
-    children: ReactNode
-}
+    children: ReactNode;
+};
 
 export default function SpotWrapper(props: Props) {
-    return <div {...props} className="grid grid-cols-1 divide-y sm:bg-slate-50 p-0 sm:grid-cols-2 sm:gap-3 sm:divide-none sm:p-3 lg:grid-cols-3 xl:grid-cols-4"></div>
+    return (
+        <div
+            {...props}
+            className="grid grid-cols-1 divide-y p-0 sm:grid-cols-2 sm:gap-3 sm:divide-none sm:bg-slate-50 sm:p-3 lg:grid-cols-3 xl:grid-cols-4"
+        ></div>
+    );
 }
