7291a2fc3678848fe0b294e7c5d8d9b0911c7e04
ayu0616 <ayumuogawa0616@gmail.com>
2022-12-28 11:03:31 +0900

    背景色の変更

--- components/common/modal/ModalHeader.tsx
+++ components/common/modal/ModalHeader.tsx
@@ -7,7 +7,7 @@ export default (props: {
     closeButtonOnClick?: () => void;
 }) => {
     return (
-        <div className="sticky top-0 flex justify-between rounded-t-lg border-b bg-white p-4">
+        <div className="sticky top-0 flex justify-between rounded-t-lg border-b bg-slate-50 p-4">
             <H2>{props.children}</H2>
             <ModalCloseButton onClick={props.closeButtonOnClick} />
         </div>
